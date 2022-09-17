50c1e2a4200603d3df8309d5a9390e27187a5c7e
shopi <shopin_va@mail.ru>
2022-09-17 22:39:20 +0700

    training dict

---
+++ dict.py
@@ -0,0 +1,11 @@
+info1 = {'name': 'Bob',
+        'age': 25,
+        'job': 'Dev'}
+
+info2 = {'age': 30,
+        'city': 'New York',
+        'email': 'bob@web.com'}
+
+info1 |= info2
+
+print(info1)
