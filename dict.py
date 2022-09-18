6682812f5bd3730a23c3278327bcca4e1e38c910
shopi <shopin_va@mail.ru>
2022-09-19 00:17:12 +0700

    training dict

--- dict.py
+++ dict.py
@@ -1,11 +1,4 @@
-info1 = {'name': 'Bob',
-        'age': 25,
-        'job': 'Dev'}
+numbers = [34, 10, -4, 6, 10, 23, -90, 100, 21, -35, -95, 1, 36, -38, -19, 1, 6, 87]
 
-info2 = {'age': 30,
-        'city': 'New York',
-        'email': 'bob@web.com'}
-
-info1 |= info2
-
-print(info1)
+result = {i: numbers[i]**2 for i in range(len(numbers))}
+print(result)
